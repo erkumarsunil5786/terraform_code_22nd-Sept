9258fc0a7bb67d723676048c67a10d0ba41dd996
Sunil Kumar <erkumarsunil5786@gmail.com>
2025-09-22 20:01:13 +0530

    RG Code Added

--- main.tf
+++ main.tf
@@ -1,1 +1,17 @@
-terraform {}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg"{
+    name = "sunil_rg"
+    location = "canada central"
+
+}
